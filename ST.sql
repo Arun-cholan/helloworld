7c61aac40834dcd4c7ee4221a972424211985fbf
unknown <arunkumar@cholantours.in>
2020-12-23 15:42:21 +0530

    initial commit

---
+++ ST.sql
@@ -0,0 +1,27 @@
+select *From ClientRegistration
+select *from ArrivalDeparture
+select *From TransportBooking
+select *From HotelBooking
+select *from GuideRequest
+select *From EntranceRequest
+select *from ActivityRequest
+select *From Itinerary
+
+--1 - New Request
+--2 - Proceed 
+--3 - Pending
+--4 - Confirmed
+--5 - Cancelled
+--6 - Deleted
+
+
+--TRUNCATE TABLE ClientRegistration
+--TRUNCATE TABLE ArrivalDeparture
+--TRUNCATE TABLE TransportBooking
+--TRUNCATE TABLE HotelBooking
+--TRUNCATE TABLE GuideRequest
+--TRUNCATE TABLE EntranceRequest
+--TRUNCATE TABLE ActivityRequest
+--TRUNCATE TABLE Itinerary
+
+
